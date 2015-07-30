12153a75fdc7b66268eabf34405e5b7fb5703202
ljc <liujunchaofoolish@163.com>
2015-07-30 22:39:05 +0800

    初始化项目！

---
+++ weChat/weChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weChat
+//
+//  Created by Ljson on 15/7/30.
+//  Copyright (c) 2015年 Ljson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
